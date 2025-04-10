a27a4ac715ae8f21566d4903cbf8b3b1359ff458
Justin Denning <jude9363@colorado.edu>
2025-04-10 00:15:07 +0000

    Basic framework for connecting, pairing, enabling indications, receiving indications, and writing toolbox states back

--- bt/btctl.sh
+++ bt/btctl.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ADDR="90:35:EA:0C:E4:91"
 
--- bt/data_rcvd.sh
+++ bt/data_rcvd.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "$2" > "$1.txt"
+# $2 is data received
+
+# output the new tbstate to the correct file
+echo -n "34 00 00 01 00 00 02 00 00 00 41 42 41 42 41 42 43" > tbstate
